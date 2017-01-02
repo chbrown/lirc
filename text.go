b8af9c5a3b19434562b6668ca71f5a2d6b1d6940
Christopher Brown <io@henrian.com>
2017-01-01 23:42:54 -0600

    Colorize text output

--- text.go
+++ text.go
@@ -2,33 +2,49 @@ package lirc
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/sorcix/irc"
-	"log"
+	"io"
+	"os"
+)
+
+var (
+	red     = color.New(color.FgRed).SprintFunc()
+	green   = color.New(color.FgGreen).SprintFunc()
+	yellow  = color.New(color.FgYellow).SprintFunc()
+	blue    = color.New(color.FgBlue).SprintFunc()
+	magenta = color.New(color.FgMagenta).SprintFunc()
+	cyan    = color.New(color.FgCyan).SprintFunc()
+	// with backgrounds:
+	whiteOnBlack = color.New(color.FgBlack, color.BgWhite).SprintFunc()
+	blackOnWhite = color.New(color.FgWhite, color.BgBlack).SprintFunc()
 )
 
 type TextListener struct {
 	Listener
+	w io.Writer
 }
 
-// no options
 func NewTextListener() Listener {
-	return &TextListener{}
+	return &TextListener{w: os.Stdout}
 }
 
 func fmtPrefix(p *irc.Prefix) string {
 	if p == nil {
 		return "∅"
 	}
-	return fmt.Sprintf("%s!%s@%s", p.Name, p.User, p.Host)
+	return fmt.Sprintf("%s!%s@%s", red(p.Name), magenta(p.User), p.Host)
 }
 
-func fmtMessage(m *irc.Message) string {
-	return fmt.Sprintf("%s %s %v: %s", fmtPrefix(m.Prefix), m.Command, m.Params, m.Trailing)
+func (listener *TextListener) printMessage(m *irc.Message, direction string) {
+	prefix := fmtPrefix(m.Prefix)
+	message := fmt.Sprintf("%s %s %s: %s", prefix, yellow(m.Command), cyan(m.Params), m.Trailing)
+	fmt.Fprintf(listener.w, "%s %s %s\n", direction, timestamp(), message)
 }
 
-func (_ *TextListener) Incoming(m *irc.Message) {
-	log.Printf("-> %s   \n", fmtMessage(m))
+func (listener *TextListener) Incoming(m *irc.Message) {
+	listener.printMessage(m, whiteOnBlack("<-"))
 }
-func (_ *TextListener) Outgoing(m *irc.Message) {
-	log.Printf("   %s ->\n", fmtMessage(m))
+func (listener *TextListener) Outgoing(m *irc.Message) {
+	listener.printMessage(m, blackOnWhite("->"))
 }
